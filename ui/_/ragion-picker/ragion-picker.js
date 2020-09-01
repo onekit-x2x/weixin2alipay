440f4d7cfacd6e5b2aa3a80d2421d73d7e1f9a65
zhangjinva <zhangjin_va@qq.com>
2020-09-01 17:48:33 +0800

    v1.0.42

--- ui/_/ragion-picker/ragion-picker.js
+++ ui/_/ragion-picker/ragion-picker.js
@@ -6,74 +6,116 @@ Component({
     onekitStyle: "",
     onekitClass: "",
     onekitId: "",
-    headerText: "",
+    headerText: null,
     disabled: false,
     value: ['北京市', '北京市', '东城区'],
-    "custom-item": ""
+    customItem: ""
   },
   data: {
     provinces,
-    ragion:{value:[],code:[],postcode:null}
+    ragion: { value: [], code: [], postcode: null }
   },
-  didMount(){
+  didMount() {
     const value = this.props.value;
-    this.data.ragion.value = value;
     this.init(value);
   },
   methods: {
-    init(value){
-    for (var p = 0; p < this.data.provinces.length; p++) {
+    init(value) {
+      for (var p = 0; p < this.data.provinces.length; p++) {
         if (this.data.provinces[p].name == value[0]) {
-          this.setData({ provinceIndexes: [p] });
-          const province = this.provinceChange(p);
-          this.data.ragion.code[0] = province.id;
+          const i = [this.props.customItem ? (p + 1) : p];
+          this.setData({ provinceIndexes: i });
+          this.provinceChange(p);
           break;
         }
       }
       for (var c = 0; c < this.data.citys.length; c++) {
         if (this.data.citys[c].name == value[1]) {
-          this.setData({ cityIndexes: [c] });
-          const city = this.cityChange(c);
-          this.data.ragion.code[1] = city.id;
+          const i = [this.props.customItem ? (c + 1) : c];
+          this.setData({ cityIndexes: i });
+          this.cityChange(c);
           break;
         }
       }
       for (var t = 0; t < this.data.towns.length; t++) {
         if (this.data.towns[t].name == value[2]) {
-          this.setData({ townIndexes: [t] });
-          const town = this.townChange(t);
-          this.data.ragion.code[2] = town.id;
-          this.data.ragion.postcode = town.zipcode;
+          const i = [this.props.customItem ? (t + 1) : t];
+          this.setData({ townIndexes: i });
+          this.townChange(t);
           break;
         }
       }
     },
     provinceChange(index) {
+      if (index < 0) {
+        this.data.ragion.value[0] = this.props.customItem;
+        this.data.ragion.code = [];
+        this.setData({ citys: [], cityIndexes: [0] })
+        return;
+      }
       const province = this.data.provinces[index];
       const citys = allCitys[province.id];
-      this.setData({ citys,cityIndexes:[0] })
       //
-      return province;
+      this.data.ragion.value[0] = province.name;
+      this.data.ragion.code[0] = province.id;
+      //
+      const i = this.props.customItem ? 0 : 1;
+      this.setData({ citys, cityIndexes: [i] })
     },
     cityChange(index) {
+      if (index < 0) {
+        this.data.ragion.value[1] = this.props.customItem;
+        switch (this.data.ragion.code.length) {
+          case 0:
+            break;
+          case 1:
+          case 2:
+          case 3:
+            this.data.ragion.code = [this.data.ragion.code[0]];
+            break;
+        }
+        this.setData({ towns: [], townIndexes: [0] })
+        return;
+      }
       const city = this.data.citys[index];
       const towns = allTowns[city.id];
-      this.setData({ towns,townIndexes:[0] })
       //
-      return city;
+      this.data.ragion.value[1] = city.name;
+      this.data.ragion.code[1] = city.id;
+      //
+      const i = this.props.customItem ? 0 : 1;
+      this.setData({ towns, townIndexes: [i] })
     },
     townChange(index) {
+      if (index < 0) {
+        this.data.ragion.value[2] = this.props.customItem;
+        switch (this.data.ragion.code.length) {
+          case 0:
+          this.data.ragion.code = [];
+            break;
+          case 1:
+            this.data.ragion.code = [this.data.ragion.code[0]];
+            break;
+          case 2:
+            this.data.ragion.code = [this.data.ragion.code[0],this.data.ragion.code[1]];
+            break;
+        }
+        this.data.ragion.postcode = null;
+        return;
+      }
       const town = this.data.towns[index];
       //
-      return town;
+      this.data.ragion.value[2] = town.name;
+      this.data.ragion.code[2] = town.id;
+      this.data.ragion.postcode = town.zipcode;
     },
     ragion_show() {
       if (this.props.disabled) {
         return;
       }
-     const value = this.data.ragion.value;
-     this.init(value);
-      this.setData({ show: true })
+      const value = this.data.ragion.value;
+      this.init(value);
+      this.setData({ show: true,provinceIndexes:this.data.provinceIndexes })
     },
     ragion_cancle() {
       this.setData({ show: false })
@@ -84,18 +126,26 @@ Component({
     ragion_confirm() {
       this.setData({ show: false })
       if (this.props.onChange) {
-        this.props.onChange({ detail:  this.data.ragion });
+        this.props.onChange({ detail: this.data.ragion });
       }
     },
     province_change(e) {
-      this.provinceChange(e.detail.value);
-      this.cityChange(0);
+      const index = e.detail.value[0];
+      this.data.provinceIndexes = [index];
+      this.provinceChange(this.props.customItem && index == 0 ? -1 : (this.props.customItem ? (index - 1) : index));
+      this.cityChange(this.props.customItem ? -1 : 0);
+      this.townChange(this.props.customItem ? -1 : 0);
     },
     city_change(e) {
-      this.cityChange(e.detail.value);
+      const index = e.detail.value[0];
+      this.data.cityIndexes = [index];
+      this.cityChange(this.props.customItem && index == 0 ? -1 : (this.props.customItem ? (index - 1) : index));
+      this.townChange(this.props.customItem ? -1 : 0);
     },
     town_change(e) {
-      this.townChange(e.detail.value);
+      const index = e.detail.value[0];
+      this.data.townIndexes = [index];
+      this.townChange(this.props.customItem && index == 0 ? -1 : (this.props.customItem ? (index - 1) : index));
     }
   },
 });

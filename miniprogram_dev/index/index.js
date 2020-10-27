fdfe26c8fa8bd4b6003889d40100244cb012dff6
lyx0lwz <69792963+lyx0lwz@users.noreply.github.com>
2020-10-27 09:39:54 +0800

    .

--- miniprogram_dev/index/index.js
+++ miniprogram_dev/index/index.js
@@ -1,8 +1,25 @@
-import {OnekitPage,wx} from "../weixin2alipay"
+import {OnekitPage} from '../weixin2alipay/index';
+import {wx} from '../weixin2alipay/index';
+global = {};
+var initData = 'this is first line\nthis is second line';
+var extraLine = [
+];
 OnekitPage({
-  data: {},
-  onLoad() {
-    wx.showLoading({
+    data:{
+        text:initData
+    },
+    add:function(e){
+        extraLine.push('other line');
+        this.setData({
+        text:(((initData + '\n')) + extraLine.join('\n'))
     });
-  },
-})
+    },
+    remove:function(e){
+        if((extraLine.length > 0)){
+        extraLine.pop();
+        this.setData({
+            text:(((initData + '\n')) + extraLine.join('\n'))
+        });
+    }
+    }
+});

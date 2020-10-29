372a2f0bcc1a1159d09ee4f58c6468183f8b4ead
lyx0lwz <69792963+lyx0lwz@users.noreply.github.com>
2020-10-29 11:54:50 +0800

    .

--- miniprogram_dev/index/index.js
+++ miniprogram_dev/index/index.js
@@ -1,61 +1,53 @@
 import {OnekitPage} from '../weixin2alipay/index';
 import {wx} from '../weixin2alipay/index';
 global = {};
+function getRandomColor(){
+    const rgb = [
+    ];
+    for(var i = 0;(i < 3);++i){
+        var color = Math.floor((Math.random() * 256)).toString(16);
+        color = (color.length == 1)?('0' + color):color;
+        rgb.push(color);
+    };
+    return ('#' + rgb.join(''));
+};
 OnekitPage({
-    data:{
-        array:[
-            {
-                mode:'scaleToFill',
-                text:'scaleToFill：不保持纵横比缩放图片，使图片完全适应'
-            },
-            {
-                mode:'aspectFit',
-                text:'aspectFit：保持纵横比缩放图片，使图片的长边能完全显示出来'
-            },
-            {
-                mode:'aspectFill',
-                text:'aspectFill：保持纵横比缩放图片，只保证图片的短边能完全显示出来'
-            },
-            {
-                mode:'top',
-                text:'top：不缩放图片，只显示图片的顶部区域'
-            },
-            {
-                mode:'bottom',
-                text:'bottom：不缩放图片，只显示图片的底部区域'
-            },
-            {
-                mode:'center',
-                text:'center：不缩放图片，只显示图片的中间区域'
-            },
-            {
-                mode:'left',
-                text:'left：不缩放图片，只显示图片的左边区域'
-            },
-            {
-                mode:'right',
-                text:'right：不缩放图片，只显示图片的右边边区域'
-            },
-            {
-                mode:'top left',
-                text:'top left：不缩放图片，只显示图片的左上边区域'
-            },
-            {
-                mode:'top right',
-                text:'top right：不缩放图片，只显示图片的右上边区域'
-            },
-            {
-                mode:'bottom left',
-                text:'bottom left：不缩放图片，只显示图片的左下边区域'
-            },
-            {
-                mode:'bottom right',
-                text:'bottom right：不缩放图片，只显示图片的右下边区域'
-            }
-        ],
-        src:'../resources/cat.jpg'
+    onReady:function(res){
+        this.videoContext = wx.createVideoContext('myVideo');
     },
-    imageError:function(e){
-        console.log('image3发生error事件，携带值为',e.detail.errMsg);
+    inputValue:'',
+    data:{
+        src:'',
+        danmuList:[
+            {
+                text:'第 1s 出现的弹幕',
+                color:'#ff0000',
+                time:1
+            },
+            {
+                text:'第 3s 出现的弹幕',
+                color:'#ff00ff',
+                time:3
+            }
+        ]
+    },
+    bindInputBlur:function(e){
+        this.inputValue = e.detail.value;
+    },
+    bindSendDanmu:function(){
+        this.videoContext.sendDanmu({
+        text:this.inputValue,
+        color:getRandomColor()
+    });
+    },
+    bindPlay:function(){
+        this.videoContext.play();
+    },
+    bindPause:function(){
+        this.videoContext.pause();
+    },
+    videoErrorCallback:function(e){
+        console.log('视频错误信息:');
+        console.log(e.detail.errMsg);
     }
 });
--- src/ui/image/image.js
+++ src/ui/image/image.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable camelcase */
 import {rel2abs} from '../../js/TheKit'
 import onekit_behavior from '../../behavior/onekit_behavior'
@@ -32,14 +33,57 @@ Component({
         this.props.onLoad(e)
       }
     },
-    image_tap() {
+    onShareAppMessage() {
+      return {
+        title: '分享 View 组件',
+        desc: 'View 组件很通用',
+        path: 'weixin2alipay/ui/image/image',
+      }
+    },
+    image_longTap() {
       if (this.props.showMenuByLongpress === true) {
-        setTimeout(() => {
-          my.showActionSheet({
-            items: ['发送给朋友', '收藏', '保存图片', '识别图片的小程序码'],
-            cancelButtonText: '取消',
-          })
-        }, 1000)
+        my.showActionSheet({
+          items: ['发送给朋友', '收藏', '保存图片', '识别图片的小程序码'],
+          cancelButtonText: '取消',
+          success: (res) => {
+            const btn = res.index === -1 ? '取消' : res.index
+            // my.alert({
+            //   title: `你点了${btn}按钮`
+            // })
+            console.log(`${btn}`)
+            switch (`${btn}`) {
+              case 0:
+                console.log('000000000')
+                this.onShareAppMessage()
+                break
+              case 1:
+                console.log('111111111')
+                break
+              case 2:
+                console.log('222222222')
+                my.saveImage({
+                  url: this.props.src,
+                  showActionSheet: true,
+                  success: () => {
+                    my.alert({
+                      title: '保存成功',
+                    })
+                  },
+                })
+                break
+              case 3:
+                console.log('3333333333')
+                my.ix.onCodeScan((r) => {
+                  if (r.success) {
+                    console.log('code: ' + r.code)
+                  }
+                })
+                break
+              default:
+                break
+            }
+          }
+        })
         this.setData({
           showMenuByLongpress: this.props.showMenuByLongpress
         })
